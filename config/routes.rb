f8ed1b48e05c182d492f54c7f00cc704b8e861b8
Habtamu210 <beleyaha@gmail.com>
2023-07-12 03:33:23 -0700

    Create hello rails applications

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+  root 'root#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :messages, only: [:index], defaults: { format: 'json' } do
+        collection do
+          get :random
+        end
+      end
+    end
+  end
 end
